ad0713c9d147d3c8445db5b28c2aa37ec73b64ea
Carlos Henrique Galves Junior <chjunior1@gmail.com>
2024-01-10 15:44:32 -0300

    recomeçando o projeto do zero

---
+++ web/tailwind.config.js
@@ -0,0 +1,18 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    //"./src/**/*.{js,ts,jsx,tsx}",
+    "./src/**/*.tsx",
+    "./index.html"
+  ],
+  theme: {
+    extend: {
+      colors: {
+        background: '#09090A',
+      },
+
+    },
+  },
+  plugins: [],
+}
+
